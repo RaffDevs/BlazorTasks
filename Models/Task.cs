2a8a6eed614f3bf51d416668f2eea6e523f6b56f
RaffDevs <rveronezdeveloper@gmail.com>
2024-03-13 17:51:10 -0300

    first commit

---
+++ Models/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace TarefasBlazor.Models;
+
+public class Task
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+    public bool Done { get; set; }
+    public DateTime CreatedAt { get; set; }
+}

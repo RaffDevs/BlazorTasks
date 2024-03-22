0aadff41a92f5c112c99283b1c6c7cba02e16cd9
RaffDevs <rveronezdeveloper@gmail.com>
2024-03-22 17:53:39 -0300

    feat: impl tasks logic and UI

---
+++ Services/TaskService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+using Task = TarefasBlazor.Models.Task;
+
+namespace TarefasBlazor.Services;
+
+public class TaskService
+{
+    private readonly HttpClient _httpClient;
+
+    public TaskService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<Task>> GetTasks()
+    {
+        var request = 
+            await _httpClient.GetAsync("https://raw.githubusercontent.com/RaffDevs/MyMocks/main/tasks.json");
+
+        if (!request.IsSuccessStatusCode) throw new HttpRequestException();
+        
+        var response = await request.Content.ReadFromJsonAsync<IEnumerable<Task>>();
+        return response;
+
+    }
+}

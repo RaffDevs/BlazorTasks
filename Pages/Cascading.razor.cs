c5ee98fe2fef63fb0fbe7e4cd29fc3e0c613fe95
RaffDevs <rveronezdeveloper@gmail.com>
2024-04-10 17:53:55 -0300

    feats

---
+++ Pages/Cascading.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TarefasBlazor.Pages;
+
+public class CascadingBase : ComponentBase
+{
+    protected string Message = "Hello fellas!";
+}
---
+++ Pages/Home.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using TarefasBlazor.Services;
+using TaskModel = TarefasBlazor.Models.Task;
+
+namespace TarefasBlazor.Pages;
+
+public class HomeBase : ComponentBase
+{
+    [Inject]
+    private TaskService _taskService { get; set; }
+    protected List<TaskModel> _tasks = [];
+    protected bool _loading = true;
+    protected bool _hasErrors = false;
+    protected string? _description = string.Empty;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var data = await _taskService.GetTasks();
+        _tasks = data.ToList();
+        _loading = false;
+    }
+
+    protected void AddTask()
+    {
+        if (String.IsNullOrEmpty(_description))
+        {
+            _hasErrors = true;
+            return;
+        }
+
+        var task = new TaskModel
+        {
+            Id = Guid.NewGuid(),
+            Description = _description,
+            CreatedAt = DateTime.Now,
+            Done = false
+        };
+
+        _tasks.Add(task);
+        _description = string.Empty;
+        _hasErrors = false;
+    }
+    
+    protected void DeletaTarefa(TaskModel task)
+    {
+        _tasks.Remove(task);
+    }
+}
